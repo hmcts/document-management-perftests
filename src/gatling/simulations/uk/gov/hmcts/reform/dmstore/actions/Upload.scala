2e79f960953a52ce5ae624634a8619953247b17d
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-01-30 14:11:28 +0000

    Add large file testing support

--- src/gatling/simulations/uk/gov/hmcts/reform/dmstore/actions/Upload.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/dmstore/actions/Upload.scala
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.dmstore.actions
 
+import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
@@ -7,25 +8,36 @@ import uk.gov.hmcts.reform.dmstore.actions.setup.Document
 
 object Upload {
 
+  val config = ConfigFactory.load()
+
+  private val testStandardToLargeRatio = config.getInt("params.testStandardToLargeRatio")
+  private val standardProbability = 100.0 / (testStandardToLargeRatio + 1) * testStandardToLargeRatio
+  private val largeProbability = 100.00 - standardProbability
+
+
   val upload: ChainBuilder =
-    feed(Document.documentsFeeder)
-      .exec(
-        http("Upload document")
-          .post(url = "/documents")
-          .headers(Map(
-            "ServiceAuthorization" -> "Bearer ${service_token}",
-            "user-id" -> "auto.test.cnp@gmail.com",
-          ))
-          .bodyPart(
-            RawFileBodyPart("files", Document.documentsPath.concat("${document_file}"))
-              .contentType(session => session("document_file").validate[String].map(s => Document.contentType(s)))
-              .fileName("${document_file}")).asMultipartForm
-          .formParam("classification", "PUBLIC")
-          .check(status is 200, jsonPath("$._embedded.documents[0]._links.self.href").saveAs("fileUrl")))
-      .exec(session => {
-          val fileUrl = session("fileUrl").as[String]
-          val fileId = fileUrl.substring(fileUrl.lastIndexOf('/') + 1)
-          Document.uploadedDocuments += fileId
-          println(Document.uploadedDocuments)
-          session })
+    randomSwitch(
+      standardProbability -> feed(Document.documentsFeeder),
+      largeProbability -> feed(Document.largeDocumentsFeeder),
+    )
+    .exec(
+      http("Upload document")
+        .post(url = "/documents")
+        .headers(Map(
+          "ServiceAuthorization" -> "Bearer ${service_token}",
+          "user-id" -> "auto.test.cnp@gmail.com",
+        ))
+        .bodyPart(
+          RawFileBodyPart("files", Document.documentsPath.concat("${document_file}"))
+            .contentType(session => session("document_file").validate[String].map(s => Document.contentType(s)))
+            .fileName("${document_file}")).asMultipartForm
+        .formParam("classification", "PUBLIC")
+        .check(status is 200, jsonPath("$._embedded.documents[0]._links.self.href").saveAs("fileUrl")))
+    .exec(session => {
+      val fileUrl = session("fileUrl").as[String]
+      val fileId = fileUrl.substring(fileUrl.lastIndexOf('/') + 1)
+      Document.uploadedDocuments += fileId
+      println(Document.uploadedDocuments)
+      session })
+
 }
